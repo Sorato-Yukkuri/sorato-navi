86b2ead266522ee62134cc735738ec42ba8d9691
Sorato-Yukkuri <yuyusesabuchanneru@gmail.com>
2026-04-25 20:09:32 +0900

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // GitHub Pages対応
+  base: '/sorato-nav/', // GitHub Pagesリポジトリ名
 })
